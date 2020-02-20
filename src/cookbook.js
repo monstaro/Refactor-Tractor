0d3559ea510d8e2cef85f7acf46564c691aa36a7
John Watterson <john@breakthrewbrew.com>
2020-02-20 15:48:26 -0700

    Add methods to cookbook class to find and filter recipes

--- src/cookbook.js
+++ src/cookbook.js
@@ -1,24 +1,43 @@
 const Recipe = require('./recipe');
 class Cookbook {
-  constructor(recipes) {
+  constructor(recipes, ingredientsData) {
+    this.allIngredients = ingredientsData;
     this.recipes = this.populateRecipes(recipes);
   }
 
   populateRecipes(recipes) {
-    // console.log(recipes[0]);
+    // console.log(this.allIngredients);
     return recipes.map(item => {
-      return new Recipe(item);
+      let currentRecipe = new Recipe(item);
+      // console.log(currentRecipe);
+      currentRecipe.identifyIngredients(this.allIngredients);
+      return currentRecipe;
     });
   }
 
   findRecipe(id) {
-    console.log(this.recipes);
     return this.recipes.find(recipe => recipe.id === id);
   }
 
+  filterTags(tags, str) {
+    let found = tags.filter(tag => tag.toLowerCase().includes(str.toLowerCase()));
+    return found.length > 0;
+  }
+
+  filterIngredients(ingredients, str) {
+    let found = ingredients.filter(ingredient => ingredient.name.toLowerCase().includes(str.toLowerCase()));
+    return found.length > 0;
+  }
+
   filterRecipes(str) {
-    return this.recipes.filter(recipe => {
-      return 
+    return this.recipes.filter((recipe, index) => {
+      let recipeName = recipe.name;
+      let ingredients = recipe.ingredients;
+      let tags = recipe.tags;
+      if (recipeName.includes(str) || this.filterIngredients(ingredients, str) || this.filterTags(tags, str)) {
+        return recipe;
+      }
+
     });
   }
 }
--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -1,12 +1,16 @@
 import { expect } from 'chai';
 import Cookbook from '../src/cookbook.js';
 import Recipe from '../src/recipe.js';
+import ingredientsData from '../src/data/ingredient-data';
 import data from '../src/data/recipe-data.js';
 
 describe('Cookbook', function () {
   let cookbook;
-  beforeEach(function(){
-    cookbook = new Cookbook(data);
+  let sampleRecipe;
+  beforeEach(function () {
+    cookbook = new Cookbook(data, ingredientsData);
+    sampleRecipe = new Recipe(data[1]);
+    sampleRecipe.identifyIngredients(ingredientsData);
   });
 
   it('is a cookbook', function () {
@@ -14,16 +18,16 @@ describe('Cookbook', function () {
   });
 
   it('should be able to add all recipes to the cookbook', function(){
-    expect(cookbook.recipes[1]).to.deep.equal(new Recipe(data[1]));
+    expect(cookbook.recipes[1]).to.deep.equal(sampleRecipe);
   });
 
 
   it('should be able to find a recipe', function(){
-    expect(cookbook.findRecipe(678353)).to.deep.equal(data[1]);
+    expect(cookbook.findRecipe(678353)).to.deep.equal(sampleRecipe);
   });
 
   it('should be able to filter recipes by ingredient', function(){
-
+    expect(cookbook.filterRecipes('apple')).to.deep.equal([cookbook.recipes[1], cookbook.recipes[4], cookbook.recipes[8], cookbook.recipes[12], cookbook.recipes[27], cookbook.recipes[48]]);
   });
 
 });
