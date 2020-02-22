b94ddfd048d69d6015c585c048d0887cb2143104
monstaro <monstaro32@gmail.com>
2020-02-22 15:17:14 -0700

    Adds method to determine amount quantity needed

--- src/pantry.js
+++ src/pantry.js
@@ -1,6 +1,57 @@
+// const Recipe = require('./recipe');
+
+
+
+
 class Pantry {
-  constructor() {
-    
+  constructor(userInfo) {
+    this.pantry = userInfo.pantry
+  }
+  
+
+  comparePantryToRecipe(recipe) {
+    let recipeIngredients = recipe.ingredients.reduce((allIngredients, ingredient) => {
+      allIngredients.push(ingredient.name)
+      return allIngredients
+    }, []);
+
+
+    let matchingIngredients = recipe.ingredients.reduce((ingredientsList, ingredient) => {
+      this.pantry.forEach(item => {
+        if (!ingredientsList.includes(ingredient.name) && item.ingredient === ingredient.id) {
+          ingredientsList.push(ingredient.name)
+        }
+      })
+
+      return ingredientsList
+    }, [])
+    if (recipeIngredients.length === matchingIngredients.length) {
+      return true
+    } else {
+      return false;
+    }
+
+    // let findDiff = recipeIngredients.filter(ing => !matchingIngredients.includes(ing))
+    // console.log(findDiff)
+  }
+  determineIngredientQuantityNeeded(recipe) {
+    let pantryAmount = 0
+    let unit;
+    let amount;
+    let ingredientName;
+    let requiredAmount;
+    recipe.ingredients.forEach(ingredient => {
+      this.pantry.forEach(item => {
+        if (ingredient.id === item.ingredient) {
+          pantryAmount += item.amount;
+          unit = ingredient.quantity.unit;
+          requiredAmount = ingredient.quantity.amount
+          ingredientName = ingredient.name;
+          amount = `You have ${pantryAmount} ${unit} of ${ingredientName}, you need ${requiredAmount} ${ingredient.quantity.unit} to make this reicpe.`
+        }
+      })
+    })
+    return amount;
   }
 }
 
