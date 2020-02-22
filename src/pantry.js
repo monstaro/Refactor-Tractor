f6dbf2c029d1c880b9e03faa7b77fd9362561075
monstaro <monstaro32@gmail.com>
2020-02-22 16:17:02 -0700

    Refactors method so it only returns if the required amount for a recipe is greater than the quantity in the pantry

--- src/pantry.js
+++ src/pantry.js
@@ -8,7 +8,6 @@ class Pantry {
     this.pantry = userInfo.pantry
   }
   
-
   comparePantryToRecipe(recipe) {
     let recipeIngredients = recipe.ingredients.reduce((allIngredients, ingredient) => {
       allIngredients.push(ingredient.name)
@@ -16,42 +15,77 @@ class Pantry {
     }, []);
 
 
+  
+
     let matchingIngredients = recipe.ingredients.reduce((ingredientsList, ingredient) => {
       this.pantry.forEach(item => {
         if (!ingredientsList.includes(ingredient.name) && item.ingredient === ingredient.id) {
           ingredientsList.push(ingredient.name)
         }
       })
-
       return ingredientsList
     }, [])
+
+
+
+    // let findDiff = recipeIngredients.filter(ing => !matchingIngredients.includes(ing))
+
     if (recipeIngredients.length === matchingIngredients.length) {
       return true
     } else {
       return false;
     }
-
-    // let findDiff = recipeIngredients.filter(ing => !matchingIngredients.includes(ing))
-    // console.log(findDiff)
   }
+
+
+  
   determineIngredientQuantityNeeded(recipe) {
-    let pantryAmount = 0
-    let unit;
-    let amount;
-    let ingredientName;
-    let requiredAmount;
-    recipe.ingredients.forEach(ingredient => {
-      this.pantry.forEach(item => {
-        if (ingredient.id === item.ingredient) {
-          pantryAmount += item.amount;
-          unit = ingredient.quantity.unit;
-          requiredAmount = ingredient.quantity.amount
-          ingredientName = ingredient.name;
-          amount = `You have ${pantryAmount} ${unit} of ${ingredientName}, you need ${requiredAmount} ${ingredient.quantity.unit} to make this reicpe.`
-        }
+
+    // if (!this.matchingIngredients) {
+    //   let pantryAmount = 0
+    //   let unit;
+    //   let amount;
+    //   let ingredientName;
+    //   let requiredAmount;
+    //   recipe.ingredients.forEach(ingredient => {
+    //     this.pantry.forEach(item => {
+    //       if (ingredient.id === item.ingredient) {
+    //         pantryAmount += item.amount;
+    //         unit = ingredient.quantity.unit;
+    //         requiredAmount = ingredient.quantity.amount
+    //         ingredientName = ingredient.name;
+    //         amount = `You have ${pantryAmount} ${unit} of ${ingredientName}, you need ${requiredAmount} ${ingredient.quantity.unit} to make this reicpe.`
+    //       }
+    //     })
+    //   })
+    //   return amount;
+    // }
+
+
+    if (!this.matchingIngredients) {
+      let pantryAmount = 0
+      let unit;
+      let amount;
+      let ingredientName;
+      let requiredAmount;
+
+
+
+      recipe.ingredients.forEach(ingredient => {
+        this.pantry.forEach(item => {
+          if (ingredient.id === item.ingredient) {
+            pantryAmount += item.amount;
+            unit = ingredient.quantity.unit;
+            requiredAmount = ingredient.quantity.amount
+            ingredientName = ingredient.name;
+            amount = `You have ${pantryAmount} ${unit} of ${ingredientName}, you need ${requiredAmount} ${ingredient.quantity.unit} to make this reicpe.`
+          }
+        })
       })
-    })
-    return amount;
+      if (requiredAmount > pantryAmount) {
+        return amount;
+      }
+    }
   }
 }
 
